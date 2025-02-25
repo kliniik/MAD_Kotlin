5081d90015194da49a5948e7e7bf5706010f069b
natalia klinik <147657334+kliniik@users.noreply.github.com>
2025-02-25 14:26:50 +0100

    Update OpenStreetMapActivity.kt

--- app/src/main/java/es/upm/btb/madproject/OpenStreetMapActivity.kt
+++ app/src/main/java/es/upm/btb/madproject/OpenStreetMapActivity.kt
@@ -17,6 +17,11 @@ import android.graphics.drawable.BitmapDrawable
 import androidx.core.content.ContextCompat
 import android.content.Context
 import org.osmdroid.views.overlay.Polyline
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.VectorDrawable
+import android.graphics.drawable.Drawable
+
 
 class OpenStreetMapActivity : AppCompatActivity() {
     private val TAG = "btaOpenStreetMapActivity"
@@ -34,13 +39,36 @@ class OpenStreetMapActivity : AppCompatActivity() {
     )
     private val gymkhanaNames = listOf(
         "Tennis",
-        "Futsal outdoors",
-        "Fashion and design school",
-        "Topography school",
-        "Telecommunications school",
+        "Futsal Outdoors",
+        "Fashion and Design School",
+        "Topography School",
+        "Telecommunications School",
         "ETSISI",
         "Library",
         "CITSEM"
+    )
+
+    private val gymkhanaDescriptions = listOf(
+        "Outdoor tennis courts for practice and matches.",
+        "Open-air futsal field for small-sided games.",
+        "Institution focused on fashion and creative design studies.",
+        "School specializing in land surveying and mapping technologies.",
+        "Faculty dedicated to communication and network technologies.",
+        "School of Information Systems Engineering and Informatics.",
+        "A resource center with books, study spaces, and digital materials.",
+        "Research center for technology and applied sciences."
+    )
+
+
+    private val gymkhanaIcons = listOf(
+        R.drawable.icon_tennis,
+        R.drawable.icon_futsal,
+        R.drawable.icon_fashion,
+        R.drawable.icon_topography,
+        R.drawable.icon_telecom,
+        R.drawable.icon_etsisi,
+        R.drawable.icon_library,
+        R.drawable.icon_citsem
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,7 +84,8 @@ class OpenStreetMapActivity : AppCompatActivity() {
 
         // Configure the user agent before loading the configuration
         Configuration.getInstance().userAgentValue = "es.upm.btb.madproject"
-        Configuration.getInstance().load(applicationContext, getSharedPreferences("osm", MODE_PRIVATE))
+        Configuration.getInstance()
+            .load(applicationContext, getSharedPreferences("osm", MODE_PRIVATE))
 
         Log.d(TAG, "OSMDroid Cache Directory: ${cacheDir.absolutePath}")
 
@@ -70,7 +99,10 @@ class OpenStreetMapActivity : AppCompatActivity() {
         }
 
         val startPoint = if (location != null) {
-            Log.d(TAG, "onCreate: Location[${location.altitude}][${location.latitude}][${location.longitude}]")
+            Log.d(
+                TAG,
+                "onCreate: Location[${location.altitude}][${location.latitude}][${location.longitude}]"
+            )
             GeoPoint(location.latitude, location.longitude)
         } else {
             Log.d(TAG, "onCreate: Location is null, using default coordinates")
@@ -81,46 +113,70 @@ class OpenStreetMapActivity : AppCompatActivity() {
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.controller.setZoom(18.0)
         map.controller.setCenter(startPoint)
-        map.setMultiTouchControls(true) // Pinch-to-Zoom aktivieren
+        map.setMultiTouchControls(true)
 
         // Add starting point marker
         val marker = Marker(map)
         marker.position = startPoint
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        marker.icon = ContextCompat.getDrawable(this, android.R.drawable.ic_menu_compass) as BitmapDrawable
+        marker.icon =
+            ContextCompat.getDrawable(this, android.R.drawable.ic_delete) as BitmapDrawable
         marker.title = "My current location"
         map.overlays.add(marker)
+
         // Add list of markers
-        addGymkhanaMarkers(map, gymkhanaCoords, gymkhanaNames, this)
+        addGymkhanaMarkers(map, gymkhanaCoords, gymkhanaNames, gymkhanaIcons, gymkhanaDescriptions, this)
+        addRouteMarkers(map, gymkhanaCoords, this)
     }
 
-    private fun addGymkhanaMarkers(map: MapView, coords: List<GeoPoint>, names: List<String>, context: Context) {
-
+    private fun addGymkhanaMarkers(
+        map: MapView,
+        coords: List<GeoPoint>,
+        names: List<String>,
+        icons: List<Int>,
+        descriptions: List<String>,
+        context: Context
+    ) {
         for (i in coords.indices) {
-            val marker = Marker(map)
-            marker.position = coords[i]
-            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-            marker.icon = ContextCompat.getDrawable(context, android.R.drawable.ic_menu_compass) as BitmapDrawable
-            marker.title = names[i]
-            map.overlays.add(marker)
+            val iconSize = 50
 
-            // Add list of markers
-            //addGymkhanaMarkers(map, gymkhanaCoords, gymkhanaNames, this)
-            addRouteMarkers(map, gymkhanaCoords, gymkhanaNames, this)
+            for (i in coords.indices) {
+                val marker = Marker(map)
+                marker.position = coords[i]
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                // marker.icon = ContextCompat.getDrawable(context, android.R.drawable.ic_menu_compass) as BitmapDrawable
+
+                if (i < icons.size) {
+                    val resizedIcon = vectorToBitmapDrawable(context, icons[i], iconSize, iconSize)
+                    if (resizedIcon != null) {
+                        marker.icon = resizedIcon
+                    } else {
+                        Log.e(TAG, "addGymkhanaMarkers: Error resizing icon for ${names[i]}")
+                    }
+                } else {
+                    Log.e(TAG, "addGymkhanaMarkers: No icon for ${names[i]}")
+                }
+                //marker.icon = ContextCompat.getDrawable(context, icons[i]) as BitmapDrawable
+                marker.title = names[i]
+                marker.snippet = descriptions[i]
+                map.overlays.add(marker)
+            }
         }
+        map.invalidate()
     }
 
-    private fun addRouteMarkers(map: MapView, coords: List<GeoPoint>, names: List<String>, context: Context) {
+    private fun addRouteMarkers(map: MapView, coords: List<GeoPoint>, context: Context) {
         val polyline = Polyline()
         polyline.setPoints(coords)
-        for (i in coords.indices) {
-            val marker = Marker(map)
-            marker.position = coords[i]
-            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-            marker.icon = ContextCompat.getDrawable(context, android.R.drawable.ic_menu_compass) as BitmapDrawable
-            marker.title = names[i]
-            map.overlays.add(marker)
-        }
         map.overlays.add(polyline)
+    }
+
+    private fun vectorToBitmapDrawable(context: Context, drawableId: Int, width: Int, height: Int): BitmapDrawable? {
+        val drawable = ContextCompat.getDrawable(context, drawableId) ?: return null
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return BitmapDrawable(context.resources, bitmap)
     }
 }
